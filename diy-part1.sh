2014c4a92d99748fa50b095c1b22db014e90c10d
wengwin <77629552+wengwin@users.noreply.github.com>
2021-06-24 18:23:13 +0800

    Initial commit

---
+++ diy-part1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+#git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
+#git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+#git clone https://github.com/garypang13/smartdns-le package/smartdns-le
+#git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
