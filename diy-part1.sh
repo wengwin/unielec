eaee89f7bf7f624934a15d5f7d5fdd85f776f9f2
wengwin <77629552+wengwin@users.noreply.github.com>
2021-06-27 02:27:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,9 +13,11 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
